68e3a6b7590eed87df2ae2eacffe285a81e1305a
Noahnoah55 <giantnoah@gmail.com>
2019-08-03 14:32:50 -0400

    Main Menu Finished

---
+++ Assets/Scripts/UI/StartButton.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class StartButton : MonoBehaviour
+{
+    Sprite defaultsprite;
+    public Sprite hoversprite;
+    public string firstlevelname;
+    SpriteRenderer spr;
+    // Start is called before the first frame update
+    void Start()
+    {
+        spr = GetComponent<SpriteRenderer>();
+        defaultsprite = spr.sprite;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnMouseEnter()
+    {
+        spr.sprite = hoversprite;
+    }
+
+    void OnMouseExit()
+    {
+        spr.sprite = defaultsprite;
+    }
+
+    void OnMouseUpAsButton()
+    {
+        SceneManager.LoadScene(firstlevelname);
+    }
+}
