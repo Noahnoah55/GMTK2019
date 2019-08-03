1a1da6715bb971d5c3fc64b9c48c85170320c2ac
Noahnoah55 <giantnoah@gmail.com>
2019-08-03 13:07:30 -0400

    Camera Upgrade

---
+++ Assets/Scripts/FollowPlayer.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowPlayer : MonoBehaviour
+{
+    public GameObject player;
+    public float lerpt = .5f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 lerpedpos = Vector3.Lerp(transform.position,player.transform.position,lerpt);
+        lerpedpos.z = transform.position.z;
+        transform.position = lerpedpos;
+    }
+}
