ea0c6cb6a8222099609c7a73f29a6b6478ef839e
Noahnoah55 <giantnoah@gmail.com>
2019-08-04 07:09:14 -0400

    a

---
+++ Assets/CloseGame.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CloseGame : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(end());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    IEnumerator end()
+    {
+        yield return new WaitForSeconds(5f);
+        print("ended");
+        Application.Quit();
+    }
+}
