a7683d758844c8361d1445daf06a2b2ab0ce1c7d
Noahnoah55 <giantnoah@gmail.com>
2019-08-04 03:49:58 -0400

    Better Boomer

---
+++ Assets/BOOMER.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BOOMER : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public Sprite[] sprites;
+    int currentsprite = 0;
+    void Start()
+    {
+        InvokeRepeating("NextSprite",1f/30f,1f/30f);
+    }
+
+    // Update is called once per frame
+    void NextSprite()
+    {
+        if (currentsprite < sprites.Length)
+        {
+            GetComponent<SpriteRenderer>().sprite = sprites[currentsprite];
+            currentsprite ++;
+        }
+    }
+}
